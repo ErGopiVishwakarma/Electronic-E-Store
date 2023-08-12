d182e82f41f90cce0fe49968fd73f7e92f701d9c
Gopi vishwakarma <g2468vi@gmail.com>
2023-08-12 16:55:26 +0530

    bug fixed

--- tech-cube/src/Pages/SignUp.jsx
+++ tech-cube/src/Pages/SignUp.jsx
@@ -104,7 +104,7 @@ export default function SignUp() {
     }
 
     let check = false;
-    const data = await axios.get('http://localhost:8080/user').then(res => res.data)
+    const data = await axios.get('https://viridian-confusion-henley.glitch.me/user').then(res => res.data)
     if (data.length > 0) {
       data.forEach(el => {
         if (el.email === userData.email) {
